33de7a4452a0068e0d29abae53464ec092bee331
Sachin Sudani <sachin@xenett.com>
2025-06-11 17:25:07 +0530

    fix: student conflict error

--- backend/src/services/student/v1/add.student.service.ts
+++ backend/src/services/student/v1/add.student.service.ts
@@ -4,9 +4,16 @@ import { HttpStatus } from "../../../enums";
 import { ApiResponse } from "../../../utils/ApiResponse";
 import { asyncHandler } from "../../../utils/asyncHandler";
 import { addStudentValidator } from "./validators/add.student.validator";
+import { ApiError } from "../../../utils/ApiError";
 
 const addStudent: RequestHandler = asyncHandler(async (req, res) => {
     const payload = addStudentValidator.parse(req.body);
+
+    const studentExists = await prisma.student.findUnique({ where: { email: payload.email } });
+
+    if (studentExists) {
+        throw new ApiError(HttpStatus.CONFLICT, "Student with this email already exists");
+    }
 
     const student = await prisma.student.create({
         data: {
